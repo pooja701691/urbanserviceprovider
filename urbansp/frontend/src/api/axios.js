98b7638923eb56509558db6aa46838443c0138da
pooja701691 <pj4881266@gmail.com>
2026-06-09 14:57:27 +0530

    commited

--- urbansp/frontend/src/api/axios.js
+++ urbansp/frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://usp-m9by.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || 'https://usp-m9by.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
